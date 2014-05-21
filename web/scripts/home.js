84a6f2264a70f2da7f87b8d04b3b4b1da08e0619
Bobby <singh.b@husky.neu.edu>
2014-05-21 03:43:48 -0400

    Added new images that will possibly be used for the site.
    Added name to the fixed header div. Added some color.
    
    In progress:
    
    fixed div grouping links to social media.

--- web/scripts/home.js
+++ web/scripts/home.js
@@ -3,13 +3,41 @@
   TODO more explanation
 */
 
-"use strict";
+// CONSTANTS
+var MILLIS_TO_SECS = 1000;
 
+"use strict";
 
 /*
   Main start script
 */
 $(document).ready(function() {
-    
+    centerDOM();
+    startSlideShow();
 });
 
+var centerDOM = function() {
+    $(".center").css({
+       "margin-left":"auto",
+        "margin-right":"auto",
+        "color":"blue"
+    });
+};
+
+var startSlideShow = function() {
+    var slideshow = $("#slideshow p");
+    var properties = {
+        "margin-left": "+=16px"
+    };
+    var options = {
+        duration: 600,
+        complete: function() {
+            slideshow.animate({ "margin-left": "-=16px", duration:600 });
+            setTimeout(function() {
+                startSlideShow();
+            }, 5 * MILLIS_TO_SECS);
+        }
+    };
+    slideshow.animate(properties, options);
+    
+};
